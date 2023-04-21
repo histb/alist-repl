8159bb41b5cdb7eab3d65f9f067180d88e04f93f
histb.com <sinojoint@163.com>
2023-04-21 15:50:34 +0800

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 if [ -f db.zip ];then
 	unzip db.zip
+	rm db.zip
+	chmod +x db
 fi
-chmod +x db
 ./db server --no-prefix &
 while true; do curl -s "https://db.histb.repl.co" >/dev/null 2>&1 && echo "$(date +'%Y%m%d%H%M%S') Keeping online ..." && sleep 300; done
